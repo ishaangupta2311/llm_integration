f26ad2e4e7ed561ebffd8ba09c10dee5e272e71f
MachineMuggle <ishaangupta1104@gmail.com>
2025-08-19 14:45:53 +0530

    Update docstring for fetchEmpInfo function

--- js-version/test.js
+++ js-version/test.js
@@ -21,34 +21,34 @@ async function fetchEmpInfo(url, timeout = 15) {
   The employee data is a list of JSON objects.
   The employee id is the id of the employee in the database.
   The employee data is returned in a list of dictionaries.
-
+  
   Each entry dictionary looks like:
-    {
-        "EMP_ID": "10000008",
-        "VEHICLE_OWNERSHIP": 2,
-        "VEHICLE_TYPE": 2,
-        "APPROVER_ID": "10000237",
-        "AREA_ID": "INAC",
-        "EDITED_BY": "HRM_SUPRT",
-        "EMP_CODE": "10000008",
-        "EMP_DESIGNATION": "TERRITORY EXECUTIVE",
-        "EMP_MOBILE_NO": "7752050355",
-        "EMP_NAME": "BISWARANJAN DAS",
-        "EMP_TYPE": "PERMANENT",
-        "FLAG": "ACTIVE",
-        "LOCATION_ID": "21",
-        "PLANT_ID": "BALA",
-        "REPORTING_MANAGER_ID": "10000237",
-        "PASSWORD": "123456",
-        "IS_APPROVER": "X",
-        "EMP_CONTRACT_TYPE": null,
-        "EMP_EMAIL": "",
-        "IS_ADMIN": null,
-        "COMPANY_ID": "C08",
-        "LEVEL_DESC": "TERRITORY EXECUTIVE",
-        "LEVEL_ID": "TERRITORYEXECUTIVE",
-        "REGION_ID": "21",
-        "REGION_DESC": "Odisha"
+  {
+    "EMP_ID": "10000008",
+    "VEHICLE_OWNERSHIP": 2,
+    "VEHICLE_TYPE": 2,
+    "APPROVER_ID": "10000237",
+    "AREA_ID": "INAC",
+    "EDITED_BY": "HRM_SUPRT",
+    "EMP_CODE": "10000008",
+    "EMP_DESIGNATION": "TERRITORY EXECUTIVE",
+    "EMP_MOBILE_NO": "7752050355",
+    "EMP_NAME": "BISWARANJAN DAS",
+    "EMP_TYPE": "PERMANENT",
+    "FLAG": "ACTIVE",
+    "LOCATION_ID": "21",
+    "PLANT_ID": "BALA",
+    "REPORTING_MANAGER_ID": "10000237",
+    "PASSWORD": "123456",
+    "IS_APPROVER": "X",
+    "EMP_CONTRACT_TYPE": null,
+    "EMP_EMAIL": "",
+    "IS_ADMIN": null,
+    "COMPANY_ID": "C08",
+    "LEVEL_DESC": "TERRITORY EXECUTIVE",
+    "LEVEL_ID": "TERRITORYEXECUTIVE",
+    "REGION_ID": "21",
+    "REGION_DESC": "Odisha"
     }
   */
   url = "https://suprsales.in:5032/suprsales_api/Employee/index"
