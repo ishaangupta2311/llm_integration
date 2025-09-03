09dfbb83ec93f3da89fa694cc677f5bec71de271
MachineMuggle <ishaangupta1104@gmail.com>
2025-09-03 15:13:45 +0530

    Create LangGraph Agent file

---
+++ js-version/utils/langgraphAgent.js
@@ -0,0 +1,32 @@
+import { StateGraph, MemorySaver } from "@langchain/langgraph";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {
+  getTopDistributorsTool,
+  getMonthlySalesTool,
+} from "@/utils/agentTools";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages";
+
+
+
+const agentTools = [getTopDistributorsTool, getMonthlySalesTool];
+
+const agentModel = new ChatGoogleGenerativeAI({ temperature: 0 });
+const agentCheckpointer = new MemorySaver();
+
+const agent = createReactAgent({
+  llm: agentModel,
+  tools: agentTools,
+  checkpointSaver: agentCheckpointer,
+});
+
+const agentFinalState = await agent.invoke(
+  { messages: [new HumanMessage("Hello, how are you? my empid is 100009")] },
+  { configurable: { thread_id: "42" } }
+);
+
+console.log(agentFinalState.messages[agentFinalState.messages.length - 1].content);
+
+const agentNextState = await agent.invoke(
+  {messages: [new HumanMessage("Who is my top distributor?")]},
+)
